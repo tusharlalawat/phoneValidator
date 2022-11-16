400bec25b67530e0b3c896591cd63f28a654f6ab
tusharlalawat <tk3850132@gmail.com>
2022-11-16 10:59:00 +0530

    Added my project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { db } from './firebase';
+import { ref, onValue, update } from 'firebase/database';
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import "./main.css";
+
 
 function App() {
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [carManufacturer, setcarManufacturer] = useState("");
+  const [carModel, setcarModel] = useState("");
+  const [vehicleNumber, setvehicleNumber] = useState("");
+
+  const updatePhoneNumber = (event) => {
+    setPhoneNumber(event.target.value);
+  };
+  const updateCarManufacturer = (event) => {
+    setcarManufacturer(event.target.value);
+  };
+  const updateCarModel = (event) => {
+    setcarModel(event.target.value);
+  };
+  const updateVehicleNumber = (event) => {
+    setvehicleNumber(event.target.value);
+  };
+
+  const verifyNumber = () => {
+    const dbRef = ref(db, 'drivers');
+
+    const unsubscribe = onValue(dbRef, (snapshot) => {
+      snapshot.forEach((childSnapshot) => {
+        childSnapshot.forEach((data) => {
+          let keyName = data.key;
+          let keyVal = data.val().toString();
+          if (keyName === "phone") {
+            if (keyVal.includes(phoneNumber)) {
+              const path = "drivers/" + childSnapshot.key.toString();
+
+              update(ref(db, path), {
+                verified: true,
+              }).then(() => {
+                const Ref = "drivers/" + childSnapshot.key.toString() + "/vehicleDetails";
+                update(ref(db, Ref), {
+                  carManufacturer: carManufacturer,
+                  carModel: carModel,
+                  vehicleNumber: vehicleNumber,
+                }).then(() => {
+                  // unsubscribe();
+                }).catch((error) => { alert("An error has been occured!") });
+                unsubscribe();
+                alert("Phone number is verified!");
+              }).catch((error) => { alert("An error has been occured!") });
+            }
+          }
+        })
+      })
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='cover'>
+      <input type="text" value={phoneNumber} onChange={updatePhoneNumber} placeholder="Contact Number..."/>
+      <input type="text" value={carManufacturer} onChange={updateCarManufacturer} placeholder="Car Manufacturer..."/>
+      <input type="text" value={carModel} onChange={updateCarModel} placeholder="Car Model..."/>
+      <input type="text" value={vehicleNumber} onChange={updateVehicleNumber} placeholder="Vehicle Number..."/>
+      <Button className='btn' onClick={verifyNumber}>Submit</Button>
     </div>
   );
 }
-
 export default App;
